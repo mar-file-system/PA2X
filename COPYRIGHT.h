6658ba49d49269abc65196c30bf0c6206224b93b
Ron Croonenberg <ronc@lanl.gov>
2015-12-11 14:31:54 -0700

    bugfix in confpars

--- COPYRIGHT.h
+++ COPYRIGHT.h
@@ -1,10 +1,9 @@
-#ifndef __PA2X_COPYRIGHT_H__
-#define __PA2X_COPYRIGHT_H__
+#ifndef __MARFS_COPYRIGHT_H__
+#define __MARFS_COPYRIGHT_H__
 
 /*
 Copyright (c) 2015, Los Alamos National Security, LLC
 All rights reserved.
-
 Copyright 2015.  Los Alamos National Security, LLC. This software was produced
 under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National
 Laboratory (LANL), which is operated by Los Alamos National Security, LLC for
@@ -19,7 +18,6 @@ Additionally, redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 1. Redistributions of source code must retain the above copyright notice, this
 list of conditions and the following disclaimer.
-
 2. Redistributions in binary form must reproduce the above copyright notice,
 this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
@@ -27,7 +25,6 @@ and/or other materials provided with the distribution.
 Laboratory, LANL, the U.S. Government, nor the names of its contributors may be
 used to endorse or promote products derived from this software without specific
 prior written permission.
-
 THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,19 +35,12 @@ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 -----
 NOTE:
 -----
-At this time, PA2X is distributed as part of MarFS is released under the BSD license.
-
-MarFS was reviewed and released by LANL under Los Alamos Computer Code identifier:
-LA-CC-15-039.
-
-MarFS uses libaws4c for Amazon S3 object communication. The original version
-is at https://aws.amazon.com/code/Amazon-S3/2601 and under the LGPL license.
-LANL added functionality to the original work. The original work plus
-LANL contributions is found at https://github.com/jti-lanl/aws4c.
+pa2X is released under the BSD license.
+pa2X was reviewed and released by LANL under Los Alamos Computer Code identifier:
+LA-CC-15-XXX.
 
 GNU licenses can be found at http://www.gnu.org/licenses/.
 */
